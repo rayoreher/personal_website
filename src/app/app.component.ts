630bad5fbd4ad8388a0c85ca07d5c63855d7f233
Ray Orellana <rayoreher@gmail.com>
2024-10-12 13:28:58 +0200

    added cv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ PdfViewerModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
